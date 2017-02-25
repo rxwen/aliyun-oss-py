090fa82414490cded6c7af12802239f6fdd5d268
skycucumber@163.com <skycucumber@163.com>
2017-02-25 12:12:40 +0800

    add ls feature

--- ossclient.py
+++ ossclient.py
@@ -11,24 +11,34 @@ import utils
 
 
 def main(argv):
+
     try:
-        opts, args = getopt.getopt(argv, "f:d", ["overwrite"])
-        if len(args) != 2:
-            raise Exception()
-            pass
-        if args[1] == '.':
-            args[1] = os.path.abspath(args[1])
-    except:
+        opts, args = getopt.getopt(argv, "f:dlr", ["overwrite"])
+
+        argDict = dict(opts)
+        overwrite = '--overwrite' in argDict
+        config_file = argDict.get('-f', os.path.expanduser("~/.ossclient.yml"))
+        download = argDict.get('-d') is not None
+        cmd_ls = argDict.get('-l') is not None
+        cmd_recur = argDict.get('-r') is not None
+
+        if not cmd_ls:
+            if len(args) != 2:
+                raise Exception()
+
+            if args[1] == '.':
+                args[1] = os.path.abspath(args[1])
+        else:
+            src_path = len(args) > 0 and args[0] or ""
+    except Exception as E:
+        print E
         print "Usage:"
         print "upload:   ", sys.argv[0], "[-f config.yaml] [--overwrite] SRC_FILE FOO/BAR/DEST_FILE"
         print "download: ", sys.argv[0], "[-f config.yaml] [-d] SRC_FILE_ON_ALIYUN LOCAL/FOO/BAR/DEST_FILE"
         print "   -f default to ~/.ossclient.yml if not specified"
+        print "ls:", sys.argv[0], "-l PATH_ON_ALIYUN"
+        print "ls recursively:", sys.argv[0], "-lr PATH_ON_ALIYUN"
         return
-
-    argDict = dict(opts)
-    overwrite = '--overwrite' in argDict
-    config_file = argDict.get('-f', os.path.expanduser("~/.ossclient.yml"))
-    download = argDict.get('-d') is not None
 
     # config = yaml.load(file(config_file))
     config = utils.load_config(config_file)
@@ -40,7 +50,29 @@ def main(argv):
 
     try:
         bucket = utils.oss_get_bucket()
-        if download:
+        if cmd_ls:
+            folders = [src_path]
+            while len(folders) > 0:
+                folder = folders[0]
+                marker = None
+                while True:
+                    print "reading info of prefix:", folder
+                    list_result = bucket.list_objects(folder, marker=marker)
+                    marker = list_result.next_marker
+                    src_files = [i.key for i in list_result.object_list]
+                    for src_file in src_files:
+                        # check if this src_file is a folder
+                        if src_file[-1] not in ["/", "\\"]:
+                            print "f: ", src_file
+                        else:
+                            print "d: ", src_file
+                            if src_file not in folders and cmd_recur:
+                                folders.append(src_file)
+                    if not list_result.is_truncated:
+                        folders.remove(folder)
+                        break
+
+        elif download:
             marker = None
             while True:
                 print "reading info of ", args[0]
@@ -48,6 +80,7 @@ def main(argv):
                 marker = list_result.next_marker
                 src_files = [i.key for i in list_result.object_list]
                 for src_file in src_files:
+                    print "src_file:", src_file
                     dest_file = os.path.join(args[1], src_file)
                     dest_path = os.path.dirname(dest_file)
                     if not os.path.exists(dest_path):
@@ -66,12 +99,13 @@ def main(argv):
         else:
             if os.path.isdir(args[0]):
                 for pair in utils.list_files(args[0], args[1]):
-                    print "uploading:", pair[0], " to ", "http://%s.%s/%s"%(utils.OSS_BUCKET,utils.OSS_ENDPOINT,pair[1])
-                    utils.uploadoss_put_file(bucket, pair[0], pair[1], overwrite)
+                    print "uploading:", pair[0], " to ", "http://%s.%s/%s" % (utils.OSS_BUCKET, utils.OSS_ENDPOINT, pair[1])
+                    utils.uploadoss_put_file(
+                        bucket, pair[0], pair[1], overwrite)
                 # raise Exception("This is a folder!")
                 pass
             else:
-                print "uploading:", args[0], " to ", "http://%s.%s/%s"%(utils.OSS_BUCKET,utils.OSS_ENDPOINT,args[1])
+                print "uploading:", args[0], " to ", "http://%s.%s/%s" % (utils.OSS_BUCKET, utils.OSS_ENDPOINT, args[1])
                 utils.oss_put_file(bucket, args[0], args[1], overwrite)
         print("Success!")
     except Exception, e:
